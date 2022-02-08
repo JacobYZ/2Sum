ce7da4789e73aedc7cfbc6604696ce97a89b698c
JacobYZ <yale_zhang_jay@163.com>
2022-02-09 02:20:55 +1100

    Added comment

--- 2Sum/2Sum.c
+++ 2Sum/2Sum.c
@@ -4,29 +4,34 @@ int main()
 {
 	float num1, num2, sum;
 	int scanf1 = 0, scanf2 = 0;
-	char c[256];
-	printf("Enter first number: ");
+	char tmp[256];
+	printf("This program adds the two numbers entered.\nExamples are as follows.\n\nPlease enter first number: -2355.442\nPlease enter second number : 89.2\n- 2355.441895 + 89.199997 = -2266.241943\n\n\n");
+
+	printf("Please enter first number: ");
+	//Loop to determine if the first input is a number
 	while (1) {
-		scanf1 = scanf(" %f", &num1);
+		scanf1 = scanf("%f", &num1);
 		if (scanf1) break;
 		else {
-			scanf("%s", &c);
-			printf("Enter first number again: ");
+			//When the input is not a number, the contents of the buffer are stored to tmp so that it can be read for the next loop.
+			scanf("%s", &tmp);
+			printf("Input error, please enter first number again: ");
 		}
 	};
 
 
-	printf("Enter second number: ");
+	printf("Please enter second number: ");
+	//Loop to determine if the second input is a number
 	while (1) {
-		scanf2 = scanf(" %f", &num2);
+		scanf2 = scanf("%f", &num2);
 		if (scanf2) break;
 		else {
-			scanf("%s", &c);
-			printf("Enter second number again: ");
+			scanf("%s", &tmp);
+			printf("Input error, please enter second number again: ");
 		}
 	};
 
 	sum = num1 + num2;
-	printf("Sum of the entered numbers: %f", sum);
+	printf("%f + %f = %f \n", num1, num2, sum);
 	return 0;
 }
